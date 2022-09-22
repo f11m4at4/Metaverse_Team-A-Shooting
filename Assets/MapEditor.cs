830b6a73bbc0ab352f921ae6f0b135323150c974
f11m4at4 <f11m4at4@gmail.com>
2022-09-22 11:31:14 +0900

    소유권 넘기기

---
+++ Assets/MapEditor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Map))]
+public class MapEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+    }
+}
